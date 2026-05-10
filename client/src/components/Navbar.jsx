cec88fb8cb4626aa3ad84660affe592a3273c4ec
onkar dudhale <dudhaleonkar@gmail.com>
2026-05-10 13:58:58 +0530

    Close login page on outside click

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import { NavLink, useNavigate } from 'react-router-dom'
 import { assets } from '../assets/assets'
 import { MenuIcon, SearchIcon, TicketPlus, XIcon } from 'lucide-react'
-import { useState } from 'react'
+import { useRef, useState, useEffect } from 'react'
 import { useClerk, UserButton, useUser } from '@clerk/clerk-react'
 import { useAppContext } from '../../context/AppContext'
 
@@ -14,12 +14,28 @@ const Navbar = () => {
 
   const { favouriteMovies } = useAppContext()
 
+  const menuRef = useRef()
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setIsOpen(false)
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [])
+
   return (
     <div className='fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-24 py-5'>
       <NavLink to='/'>
         <img src={assets.logo} alt="" />
       </NavLink>
-      <div className={`max-lg:absolute max-lg:top-0 max-lg:left-0 max-lg:font-medium max-lg:text-lg z-50 flex flex-col lg:flex-row items-center max-lg:justify-center gap-8 lg:px-8 py-3 max-lg:h-screen lg:rounded-full backdrop-blur bg-black/70 lg:bg-white/10 lg:border border-gray-300/20 overflow-hidden transition-[width] duration-300 ${isOpen ? 'max-lg:w-full' : 'max-lg:w-0'}`}>
+      <div ref={menuRef} className={`max-lg:absolute max-lg:top-0 max-lg:left-0 max-lg:font-medium max-lg:text-lg z-50 flex flex-col lg:flex-row items-center max-lg:justify-center gap-8 lg:px-8 py-3 max-lg:h-screen lg:rounded-full backdrop-blur bg-black/70 lg:bg-white/10 lg:border border-gray-300/20 overflow-hidden transition-[width] duration-300 ${isOpen ? 'max-lg:w-full' : 'max-lg:w-0'}`}>
         <XIcon className='lg:hidden absolute top-6 right-6 w-6 h-6 cursor-pointer' onClick={() => setIsOpen(!isOpen)} />
         <NavLink onClick={() => { scrollTo(0, 0); setIsOpen(false) }} to='/'>Home</NavLink>
         <NavLink onClick={() => { scrollTo(0, 0); setIsOpen(false) }} to='/movies'>Movies</NavLink>
